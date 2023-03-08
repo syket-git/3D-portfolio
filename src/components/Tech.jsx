71a43da84545c336327288f34270e6538a7cc34b
syketbb <syket@algosolver.com>
2023-03-08 21:11:12 +0600

    Technology section completed

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { SectionsWrapper } from '../HOC';
+import { BallCanvas } from './canvas';
+import { technologies } from '../constants';
 
 const Tech = () => {
   return (
-    <div>Tech</div>
-  )
-}
+    <div className="flex justify-center flex-wrap gap-10">
+      {technologies?.map((technology, index) => (
+        <div key={index} className="w-28 h-28">
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default Tech
+export default SectionsWrapper(Tech, '');
