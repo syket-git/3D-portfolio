b6f01a7e57b995be82e5fa8b75b47cfd7609c13a
jigarvakil <jigarvakil011@gmail.com>
2023-04-08 11:11:29 +0530

    fix typo of import

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { motion } from 'framer-motion';
 
 import { styles } from '../styles';
-import { SectionsWrapper } from '../hoc';
+import { SectionsWrapper } from '../HOC';
 import { fadeIn, textVariant } from '../utils/motion';
 import { testimonials } from '../constants';
 
